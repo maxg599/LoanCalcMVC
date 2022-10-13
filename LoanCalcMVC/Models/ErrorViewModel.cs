e8507eb97ac3e3e64c5f5bd1568655bf13d82868
maxg599 <109822431+maxg599@users.noreply.github.com>
2022-10-13 17:16:11 -0400

    Add project files.

---
+++ LoanCalcMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoanCalcMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
